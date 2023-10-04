62f0885e87e986c2a6aa81671364589a0f04ce08
YKyungmin <xcv556865@gmail.com>
2023-10-04 11:10:05 +0900

    맴버데이터 사진 추가하기

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,19 +1,34 @@
 import Layout from '../../common/layout/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
+	console.log('re-render');
+	const [Department, setDepartment] = useState([]);
+
 	useEffect(() => {
 		//해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
-		fetch('/DB/department.json')
+		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
-				console.log(json);
+				console.log(json.members);
+				setDepartment(json.members);
 			});
 	}, []);
 
 	return (
 		<Layout title={'Department'}>
-			<p>디파트먼트 페이지입니다.</p>
+			{Department.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={`${path}/img/${member.pic}`} alt={member.name} />
+						</div>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
